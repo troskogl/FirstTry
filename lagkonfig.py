3a45da43a2cdf4073d615f12ab6eb1b59c0b9af6
Trond Skogland <trond.skogland@atea.no>
2016-09-28 12:02:40 +0200

    nye filer

---
+++ lagkonfig.py
@@ -0,0 +1,45 @@
+# Author: Oyvind Ellefsen
+# Version 1.1
+# For Atea workshop
+
+import os
+import jinja2
+import csv
+
+
+TEMPLATE_FILENAME = 'template.j2'
+CSVDATA_FILENAME = 'data.csv'
+
+def get_data(row):
+
+	# Hent data fra csv filen
+
+	data_fields = {
+		field_name: field_value
+		for field_name, field_value in row.items()
+	}
+
+## ---------------------------------------------------------------------------
+## Sett opp Jinja2 og hent template
+## ---------------------------------------------------------------------------
+
+env = jinja2.Environment(
+	loader=jinja2.FileSystemLoader(os.getcwd()),
+	trim_blocks=True, lstrip_blocks=True)
+
+template = env.get_template(TEMPLATE_FILENAME)
+
+## ---------------------------------------------------------------------------
+## Les CSV og fyll inn data for hver linje
+## ---------------------------------------------------------------------------
+
+for row in csv.DictReader(open(CSVDATA_FILENAME)):
+	get_data(row)
+	with open(row['HOSTNAME'] + '.txt', 'w+') as f:
+		f.write(template.render(row).encode('utf-8'))
+		print ('Writing file ' + row['HOSTNAME'] + '.txt')
+		f.close()
+
+##
+## all done!
+##
